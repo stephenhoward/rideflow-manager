535c852bba54d0bfcc0b3a51dd7aad3e48d9d317
Stephen Howard <stephen@goldenguru.com>
2017-12-21 10:56:17 -0800

    add code to track js model changes on array properties

--- app/lib/model.js
+++ app/lib/model.js
@@ -1,12 +1,24 @@
 
 class Model {
     constructor(properties) {
+        let self        = this;
         this.properties = {};
         this.dirty      = {};
 
         for( let k of Object.keys(this.definition()) ) {
             if ( k in properties ) {
-                this.properties[k] = properties[k];
+
+                let v = properties[k];
+
+                // special book-keeping for list properties
+                if ( this.definition()[k] instanceof Array ) {
+
+                    v = new ModelList( v );
+                    $(v).on('list-replace', (new_list) => {
+                        self._set_list(k,new_list);
+                    });
+                }
+                this.properties[k] = v;
             }
         }
     }
@@ -17,19 +29,15 @@ class Model {
 
     _set(k,v) {
         if ( k in this.definition() ) {
+
+            // special book-keeping for list properties
+            if ( this.properties[k] instanceof ModelList ) {
+                this._set_list(k,v);
+            }
+
             if ( this.properties[k] !== v ) {
 
-                // capture the original value
-                if ( ! k in this.dirty ) {
-
-                    this.dirty[k] = this.properties[k];
-                }
-                // looks like we're reverting to the original value
-                else if ( this.dirty[k] === v ) {
-
-                    delete this.dirty[k];
-                }
-
+                this._mark_dirty( k, v, (a,b) => { return a === b } );
                 this.properties[k] = v;
 
                 $(this).trigger('model-changed');
@@ -40,11 +48,45 @@ class Model {
         return null;
     }
 
+    _set_list(k,v) {
+        let self = this;
+
+        if ( ! this.properties[k].eq(v) ) {
+
+            this._mark_dirty( k, v, (a,b) => { return a.eq(b) } );
+
+            if ( ! v instanceof ModelList ) {
+                v = new ModelList(v);
+            }
+
+            $(v).on('list-replace', (new_list) => {
+                self._set_list(k,new_list);
+            });
+
+            this.properties[k] = v;
+
+            $(this).trigger('model-changed');
+        }
+    }
+
     _get(k) {
         if ( k in this.definition() ) {
             return this.properties[k];
         }
         return null;
+    }
+
+    _mark_dirty(k,v,eq) {
+                // capture the original value
+                if ( ! k in this.dirty ) {
+
+                    this.dirty[k] = this.properties[k];
+                }
+                // looks like we're reverting to the original value
+                else if ( eq( this.dirty[k], v ) ) {
+
+                    delete this.dirty[k];
+                }
     }
 
     revert() {
@@ -74,7 +116,7 @@ class Model {
             type        : 'POST',
             dataType    : 'json',
             contentType : 'application/json; charset=utf-8',
-            data        : JSON.stringify(self.properties)
+            data        : JSON.stringify(this.dump())
         }).done( (json) => {
             for( let k of Object.keys(json) ) {
                 self._set(k,json[k]);
@@ -112,6 +154,164 @@ class Model {
         return defer.promise();
     }
 
+    dump() {
+        let data = {};
+
+        for( let k in this.properties ) {
+            let v = this.properties[k];
+
+            if ( v instanceof Model || v instanceof ModelList ) {
+                data[k] = v.dump();
+            }
+            else {
+                data[k] = v;
+            }
+        }
+
+        return data;
+    }
+
+    eq(model) {
+
+        if ( 'id' in this.definition() ) {
+            return this.id == model.id;
+        }
+
+        if ( Object.keys(this.properties).length != Object.keys(model).length ) {
+            return false;
+        }
+
+        for( k in this.properties ) {
+            let a = this.properties[k];
+            let b = model[k];
+
+            if ( a instanceof Model || a instanceof ModelList ) {
+
+                if ( ! a.eq(b) ) {
+                    return false;
+                }
+            }
+        }
+
+        return true;
+    }
+
+}
+
+class ModelList {
+    constructor(arr) {
+
+        this.items  = [];
+
+        if ( arr instanceof Array ) {
+            this.items = arr;
+        }
+        else if ( arr !== undefined ) {
+            this.items = [ arr ];
+        }
+        else {
+            this.items = [];
+        }
+
+        this._index_items();
+    }
+
+    push(item) {
+        return this._mutate( (arr) => { arr.push(item) } );
+    }
+
+    concat(items) {
+        return this._mutate( (arr) => { arr.concat(items) } );
+    }
+
+    pop() {
+        if ( this.items.length ) {
+            return this._mutate( (arr) => { return arr.pop(item) } );
+
+        }
+
+        return null;
+    }
+
+    shift() {
+        if ( this.items.length ) {
+            return this._mutate( (arr) => { return arr.shift(item) } );
+        }
+
+        return null;
+    }
+
+    unshift() {
+        return this._mutate( (arr) => { arr.unshift(item) } );
+    }
+
+    get length() {
+        return this.items.length;
+    }
+
+    eq(arr) {
+
+        if ( this.items.length != arr.length ) {
+            return false;
+        }
+
+        for( let i=0; i<this.items.length; i++ ) {
+
+            let a = this.items[i];
+            let b = arr[i];
+
+            if ( a instanceof Model ) {
+
+                if ( ! a.eq( b ) ) {
+
+                    return false;
+                }
+            }
+            else if ( a !== b ) {
+
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    dump() {
+        let arr = this._copy_items();
+
+        for( let i=0; i < arr.length; i++ ) {
+
+            if ( arr[i] instanceof Model ) {
+                arr[i] = arr[i].dump();
+            }
+        }
+
+        return arr;
+    }
+
+    _mutate(mutate) {
+
+        let arr = this._copy_items();
+
+        let result = mutate(arr);
+        let update = new ModelList(arr);
+
+        $(this).trigger('list-replace', update );
+
+        return result ? result : update;
+    }
+
+    _copy_items() {
+        return this.items.slice(0);
+    }
+
+    _index_items() {
+
+        for( let i=0; i<this.items.length; i++) {
+
+            this[i] = this.items[i];
+        }
+    }
 }
 
 module.exports = Model;
